d0d10c27342533399edc321224ae05b76867292a
Dmytro Morozov <marikwork@gmail.com>
2023-02-08 20:14:54 -0700

    hw7 task1
    gallery - done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,13 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+gallery.innerHTML = galleryItems.reduce((html, { original, preview, description}) =>
+    html + `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class= "gallery__image"
+    src="${preview}"
+    alt="${description}"/>
+    </a>
+    </div>`, '');
