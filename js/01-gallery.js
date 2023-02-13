7667c77bef42fda03297cb951a2687572eed190c
Dmytro Morozov <marikwork@gmail.com>
2023-02-13 11:22:55 -0700

    hw 7 p1
    on click showBox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,11 +4,28 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
+
 gallery.innerHTML = galleryItems.reduce((html, { original, preview, description}) =>
     html + `<div class="gallery__item">
     <a class="gallery__link" href="${original}">
     <img class= "gallery__image"
     src="${preview}"
+    data-source="${original}"
     alt="${description}"/>
     </a>
     </div>`, '');
+
+
+
+gallery.addEventListener('click', onGalleryImgClickHandler);
+
+
+function onGalleryImgClickHandler(event) { 
+    event.preventDefault();
+    if (!event.target.dataset.source) { 
+        return
+    }
+    console.log(event.target);
+    const imgBox = basicLightbox.create(`<img src="${event.target.dataset.source}"/>`);
+    imgBox.show();
+}
